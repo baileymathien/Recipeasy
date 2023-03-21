06afec6bfd4fa8471ecb573187283be3ee8c6223
Bailey Mathien <60901446+blm135@users.noreply.github.com>
2023-03-21 11:32:17 -0400

    Database logic started

--- my-app/backend/base.py
+++ my-app/backend/base.py
@@ -1,15 +1,49 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipeasy.db'
+db = SQLAlchemy(app)
+
+# MODELS
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False)
+       
+    def __repr__(self):
+       return '<User {}>'.format(self.username)
+    
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipeId = db.Column(db.String(30), nullable=False)
+    recipeName = db.Column(db.String(30), nullable=False)
+    instructions =  db.relationship('Ingredient', backref='recipe', lazy=True)
+
+
+#@app.before_first_request
+@app.cli.command('initdb')
+def createTables():
+    db.drop_all()
+    db.create_all()
+
 
 @app.route('/')
 def base():
     return redirect(url_for('login_page'))
 
-@app.route('/login')
+@app.route('/login' ,methods=['POST', 'GET'])
 def login_page():
     response_body = {
         "fname": "john",
         "lname": "smith"
     }
     return response_body
+
+@app.route('/logout',  methods=["GET", "POST"])
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
+
